23bf95e09122b9d662e072057340978611b6e561
sa1ntch <shanaurinaanya@gmail.com>
2026-05-27 17:54:48 +0300

    Update transcription_service.dart

--- lib/services/transcription_service.dart
+++ lib/services/transcription_service.dart
@@ -20,7 +20,7 @@ class AudioTranscriptionFile {
 class TranscriptionService {
   TranscriptionService({http.Client? client, Duration? requestTimeout})
     : _client = client ?? http.Client(),
-      _requestTimeout = _requestTimeout ?? const Duration(minutes: 15);
+     _requestTimeout = requestTimeout ?? const Duration(minutes: 15);
 
   static const attributionText = 'Транскрибация: Deepgram Speech-to-Text';
   static const defaultModel = 'nova-3';
